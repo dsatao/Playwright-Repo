fcdff9c007b8f429e4b77a277c22edf8bfbd40a2
dsatao <dsatao@hotmail.com>
2024-05-30 01:33:23 -0500

    fixed
    fixed some code.

--- tests/pages/home-page.ts
+++ tests/pages/home-page.ts
@@ -1,12 +1,11 @@
 import type { Locator, Page } from "@playwright/test";
 import { setMaxIdleHTTPParsers } from "http";
-
+import { CommonUtils } from "../utils/common-actions";
 export class HomePage {
     readonly page: Page;
     private searchTextBox: Locator;
     private searchBtn: Locator;
     sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 
     constructor(page: Page) {
         this.page = page;
@@ -19,14 +18,14 @@ export class HomePage {
     }
 
     private async fillSearchTxtBox(searchTxt: string) {
-       await this.searchTextBox.fill(searchTxt);
+        await this.searchTextBox.fill(searchTxt);
     }
     private async clickOnSearchBtn() {
         await this.searchBtn.dblclick();
     }
 
     async searchWithText(searchTxt: string) {
-       await this.fillSearchTxtBox(searchTxt);
+        await this.fillSearchTxtBox(searchTxt);
         await this.clickOnSearchBtn();
     }
 
