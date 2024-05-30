91f76b7568a6a48245412710b786d1382a1dd71f
dsatao <dsatao@hotmail.com>
2024-05-30 12:03:57 -0500

    Chrome Support
    Added for Chrome Browser Support.

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,10 +32,17 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+
+
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     },
+
+    /* {
+       name: 'chromium',
+       use: { ...devices['Desktop Chrome'] },
+     }, */
 
     /*{
       name: 'firefox',
